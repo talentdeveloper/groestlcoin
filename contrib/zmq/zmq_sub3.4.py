75cc85e4275873651786a50f78deb3e020aab4fa
Gruve_p <gruve-p@users.noreply.github.com>
2018-05-22 19:25:53 +0200

    Update ZMQ port

--- contrib/zmq/zmq_sub3.4.py
+++ contrib/zmq/zmq_sub3.4.py
@@ -6,12 +6,12 @@
 """
     ZMQ example using python3's asyncio
 
-    Bitcoin should be started with the command line arguments:
-        bitcoind -testnet -daemon \
-                -zmqpubrawtx=tcp://127.0.0.1:28332 \
-                -zmqpubrawblock=tcp://127.0.0.1:28332 \
-                -zmqpubhashtx=tcp://127.0.0.1:28332 \
-                -zmqpubhashblock=tcp://127.0.0.1:28332
+    Groestlcoin should be started with the command line arguments:
+        groestlcoind -testnet -daemon \
+                -zmqpubrawtx=tcp://127.0.0.1:21441 \
+                -zmqpubrawblock=tcp://127.0.0.1:21441 \
+                -zmqpubhashtx=tcp://127.0.0.1:21441 \
+                -zmqpubhashblock=tcp://127.0.0.1:21441
 
     We use the asyncio library here.  `self.handle()` installs itself as a
     future at the end of the function.  Since it never returns with the event
@@ -38,7 +38,7 @@ if not (sys.version_info.major >= 3 and sys.version_info.minor >= 4):
     print("This example only works with Python 3.4 and greater")
     sys.exit(1)
 
-port = 28332
+port = 21441
 
 class ZMQHandler():
     def __init__(self):
